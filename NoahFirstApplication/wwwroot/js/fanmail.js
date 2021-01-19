e518ca4254a9293a22165bfd551a98eceb1d39d3
Noah Johnson <jnoahjohnson@gmail.com>
2021-01-19 12:31:20 -0700

    first commit

---
+++ NoahFirstApplication/wwwroot/js/fanmail.js
@@ -0,0 +1,10 @@
+﻿
+var submit = document.getElementById("submitButton");
+
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("From: " + document.getElementById("from").value
+        + ", Email: " + document.getElementById("email").value
+        + ", Subject: " + document.getElementById("subject").value
+        + ", Message: " + document.getElementById("message").value);
+})
